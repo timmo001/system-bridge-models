cd85c992abd34577057bd4485e19510f21cb317f
Aidan Timson <aidan@timmo.dev>
2023-11-19 21:45:17 +0000

    New disk models

--- systembridgemodels/disk.py
+++ systembridgemodels/disk.py
@@ -5,14 +5,55 @@ from dataclasses import dataclass
 
 
 @dataclass
+class DiskIOCounters:
+    """Disk IO Counters"""
+
+    read_count: int
+    write_count: int
+    read_bytes: int
+    write_bytes: int
+    read_time: int
+    write_time: int
+    busy_time: int | None = None
+    read_merged_count: int | None = None
+    write_merged_count: int | None = None
+
+
+@dataclass
+class DiskUsage:
+    """Disk Usage"""
+
+    total: int
+    used: int
+    free: int
+    percent: float
+
+
+@dataclass
+class DiskPartition:
+    """Disk Partition"""
+
+    device: str
+    mount_point: str
+    fs_type: str
+    options: str
+    max_file_size: int
+    max_path_length: int
+    usage: DiskUsage | None = None
+
+
+@dataclass
+class DiskDevice:
+    """Disk Device"""
+
+    name: str
+    io_counters: DiskIOCounters|None = None
+    partitions: list[DiskPartition]
+
+
+@dataclass
 class Disk:
     """Disk"""
 
-    devices: list
-    partitions: list
-    io_counters_read_count: int | None = None
-    io_counters_write_count: int | None = None
-    io_counters_read_bytes: int | None = None
-    io_counters_write_bytes: int | None = None
-    io_counters_read_time: int | None = None
-    io_counters_write_time: int | None = None
+    devices: list[DiskDevice]
+    io_counters: DiskIOCounters
