2d47f132e0f5e6e93118b327757681db72380a90
Aidan Timson <aidan@timmo.dev>
2023-11-19 20:59:17 +0000

    Per CPU

--- systembridgemodels/cpu.py
+++ systembridgemodels/cpu.py
@@ -5,29 +5,51 @@ from dataclasses import dataclass
 
 
 @dataclass
+class CPUFrequency:
+    """CPU Frequency"""
+
+    current: float | None = None
+    min: float | None = None
+    max: float | None = None
+
+
+@dataclass
+class CPUStats:
+    """CPU Stats"""
+
+    ctx_switches: int | None = None
+    interrupts: int | None = None
+    soft_interrupts: int | None = None
+    syscalls: int | None = None
+
+
+@dataclass
+class CPUTimes:
+    """CPU Times"""
+
+    user: float | None = None
+    system: float | None = None
+    idle: float | None = None
+    interrupt: float | None = None
+    dpc: float | None = None
+
+
+@dataclass
 class CPU:
     """CPU"""
 
     count: int | None = None
-    frequency_current: float | None = None
-    frequency_min: float | None = None
-    frequency_max: float | None = None
+    frequency: CPUFrequency | None = None
+    frequency_per_cpu: list[CPUFrequency] | None = None
     load_average: float | None = None
     power_package: float | None = None
-    stats_ctx_switches: int | None = None
-    stats_interrupts: int | None = None
-    stats_soft_interrupts: int | None = None
-    stats_syscalls: int | None = None
+    power_per_cpu: list[float] | None = None
+    stats: CPUStats | None = None
     temperature: float | None = None
-    times_user: float | None = None
-    times_system: float | None = None
-    times_idle: float | None = None
-    times_interrupt: float | None = None
-    times_dpc: float | None = None
-    times_percent_user: float | None = None
-    times_percent_system: float | None = None
-    times_percent_idle: float | None = None
-    times_percent_interrupt: float | None = None
-    times_percent_dpc: float | None = None
+    times: CPUTimes | None = None
+    times_per_cpu: list[CPUTimes] | None = None
+    times_percent: CPUTimes | None = None
+    times_percent_per_cpu: list[CPUTimes] | None = None
     usage: float | None = None
+    usage_per_cpu: list[float] | None = None
     voltage: float | None = None
