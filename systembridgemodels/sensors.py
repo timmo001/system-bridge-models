bcdbf1eb0b8f992c7a72a48e443564051ceb2f6d
Aidan Timson <aidan@timmo.dev>
2023-11-19 23:33:34 +0000

    Add sensors

---
+++ systembridgemodels/sensors.py
@@ -0,0 +1,43 @@
+"""System"""
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass
+class SensorsWindowsSensor:
+    """Sensors Windows Sensor"""
+
+    id: str
+    name: str
+    type: str
+    value: float
+
+
+@dataclass
+class SensorsWindowsHardware:
+    """Sensors Windows Hardware"""
+
+    id: str
+    name: str
+    type: str
+    subhardware: list[SensorsWindowsHardware]
+    sensors: list[SensorsWindowsSensor]
+
+
+@dataclass
+class SensorsWindows:
+    """Sensors Windows"""
+
+    hardware: list[SensorsWindowsHardware] | None = None
+    nvidia: Any | None = None
+
+
+@dataclass
+class Sensors:
+    """Sensors"""
+
+    fans: Any | None = None
+    temperatures: Any | None = None
+    windows_sensors: SensorsWindows | None = None
