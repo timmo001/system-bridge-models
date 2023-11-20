bba88401cfbe44150aeab829925089f3a6162b3d
Aidan Timson <aidan@timmo.dev>
2023-11-20 20:10:18 +0000

    Memory

--- systembridgemodels/data.py
+++ systembridgemodels/data.py
@@ -2,18 +2,18 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Any
 
 from .battery import Battery
 from .cpu import CPU
-
-# from .disk import Disk
-# from .display import Display
-# from .media import Media
-# from .memory import Memory
-# from .network import Network
-# from .processes import Processes
-# from .system import System
+from .disk import Disk
+from .display import Display
+from .gpu import GPU
+from .media import Media
+from .memory import Memory
+from .network import Network
+from .processes import Processes
+from .sensors import Sensors
+from .system import System
 
 
 @dataclass
@@ -22,12 +22,12 @@ class Data:
 
     battery: Battery | None = None
     cpu: CPU | None = None
-    # disk: Disk | None = None
-    # display: Display | None = None
-    # gpu: dict[str, Any] | None = None
-    # media: Media | None = None
-    # memory: Memory | None = None
-    # network: Network | None = None
-    # processes: Processes | None = None
-    # sensors: dict[str, Any] | None = None
-    # system: System | None = None
+    disk: Disk | None = None
+    display: list[Display] | None = None
+    gpu: list[GPU] | None = None
+    media: Media | None = None
+    memory: Memory | None = None
+    network: Network | None = None
+    processes: Processes | None = None
+    sensors: Sensors | None = None
+    system: System | None = None
--- systembridgemodels/memory.py
+++ systembridgemodels/memory.py
@@ -5,17 +5,37 @@ from dataclasses import dataclass
 
 
 @dataclass
+class MemorySwap:
+    """Memory Swap"""
+
+    total: int | None = None
+    used: int | None = None
+    free: float | None = None
+    percent: float | None = None
+    sin: int | None = None
+    sout: int | None = None
+
+
+@dataclass
+class MemoryVirtual:
+    """Memory Virtual"""
+
+    total: int | None = None
+    available: int | None = None
+    percent: float | None = None
+    used: int | None = None
+    free: int | None = None
+    active: int | None = None
+    inactive: int | None = None
+    buffers: int | None = None
+    cached: int | None = None
+    wired: int | None = None
+    shared: int | None = None
+
+
+@dataclass
 class Memory:
     """Memory"""
 
-    swap_total: int | None = None
-    swap_used: int | None = None
-    swap_free: float | None = None
-    swap_percent: float | None = None
-    swap_sin: int | None = None
-    swap_sout: int | None = None
-    virtual_total: int | None = None
-    virtual_available: int | None = None
-    virtual_percent: float | None = None
-    virtual_used: int | None = None
-    virtual_free: int | None = None
+    swap: MemorySwap
+    virtual: MemoryVirtual
