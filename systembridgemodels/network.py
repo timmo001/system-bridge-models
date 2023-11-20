ec9c2793220eb01a963c7337d19faa63a38844fe
Aidan Timson <aidan@timmo.dev>
2023-11-20 20:57:37 +0000

    Update network

--- systembridgemodels/network.py
+++ systembridgemodels/network.py
@@ -5,14 +5,59 @@ from dataclasses import dataclass
 
 
 @dataclass
+class NetworkAddress:
+    """Network Address"""
+
+    address: str | None = None
+    family: str | None = None
+    netmask: str | None = None
+    broadcast: str | None = None
+    ptp: str | None = None
+
+
+@dataclass
+class NetworkStats:
+    """Network Stats"""
+
+    addresses: list[NetworkAddress] | None = None
+    isup: bool | None = None
+    duplex: str | None = None
+    speed: int | None = None
+    mtu: int | None = None
+    flags: list[str] | None = None
+
+
+@dataclass
+class NetworkConnection:
+    """Network Connection"""
+
+    fd: int | None = None
+    family: int | None = None
+    type: int | None = None
+    laddr: str | None = None
+    raddr: str | None = None
+    status: str | None = None
+    pid: int | None = None
+
+
+@dataclass
+class NetworkIO:
+    """Network IO"""
+
+    bytes_sent: int | None = None
+    bytes_recv: int | None = None
+    packets_sent: int | None = None
+    packets_recv: int | None = None
+    errin: int | None = None
+    errout: int | None = None
+    dropin: int | None = None
+    dropout: int | None = None
+
+
+@dataclass
 class Network:
     """Network"""
 
-    io_counters_bytes_sent: int | None = None
-    io_counters_bytes_recv: int | None = None
-    io_counters_packets_sent: int | None = None
-    io_counters_packets_recv: int | None = None
-    io_counters_errin: int | None = None
-    io_counters_errout: int | None = None
-    io_counters_dropin: int | None = None
-    io_counters_dropout: int | None = None
+    connections: list[NetworkConnection] | None = None
+    io: NetworkIO | None = None
+    networks: dict[str, NetworkStats] | None = None
