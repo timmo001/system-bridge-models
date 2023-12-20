6ff96a8219587e9505fc99437343f047c78de2ed
Aidan Timson <contact@timmo.xyz>
2023-12-20 11:05:49 +0000

    Add data classes for Chipset, Display, Driver, Gpus, and SensorsNVIDIA

--- systembridgemodels/modules/sensors.py
+++ systembridgemodels/modules/sensors.py
@@ -25,13 +25,75 @@ class SensorsWindowsHardware:
     subhardware: list[SensorsWindowsHardware]
     sensors: list[SensorsWindowsSensor]
 
+@dataclass
+class Chipset:
+    id: int
+    name: str
+    flags: str
+    vendor_id: int
+    vendor_name: str
+
+
+@dataclass
+class Display:
+    id: int
+    name: str
+    active: bool
+    available: bool
+    connected: bool
+    dynamic: bool
+    aspect_horizontal: int
+    aspect_vertical: int
+    brightness_current: int
+    brightness_default: int
+    brightness_max: int
+    brightness_min: int
+    color_depth: str
+    connection_type: str
+    pixel_clock: int
+    refresh_rate: int
+    resolution_horizontal: int
+    resolution_vertical: int
+
+
+@dataclass
+class Driver:
+    branch_version: str
+    interface_version: str
+    version: int
+
+
+@dataclass
+class Gpus:
+    id: int
+    name: str
+    bios_oem_revision: int
+    bios_revision: int
+    bios_version: str
+    current_fan_speed_level: int
+    current_fan_speed_rpm: int
+    driver_model: int
+    memory_available: int
+    memory_capacity: int
+    memory_maker: str
+    serial: str
+    system_type: str
+    type: str
+
+
+@dataclass
+class SensorsNVIDIA:
+    chipset: Chipset
+    displays: list[Display]
+    driver: Driver
+    gpus: list[Gpus]
 
 @dataclass
 class SensorsWindows:
     """Sensors Windows."""
 
     hardware: list[SensorsWindowsHardware] | None = None
-    nvidia: Any | None = None
+    nvidia: SensorsNVIDIA | None = None
 
 
 @dataclass
