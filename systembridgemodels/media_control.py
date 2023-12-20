ea714b319dcca01ea67c83fdcb45402affeb1ea1
Aidan Timson <contact@timmo.xyz>
2023-12-20 13:53:02 +0000

    Update Action enum name to MediaAction

--- systembridgemodels/media_control.py
+++ systembridgemodels/media_control.py
@@ -6,7 +6,7 @@ from enum import Enum
 from typing import Any
 
 
-class Action(Enum):
+class MediaAction(Enum):
     """Action."""
 
     play = "PLAY"
@@ -28,5 +28,5 @@ class Action(Enum):
 class MediaControl:
     """Media Control."""
 
-    action: Action
+    action: MediaAction
     value: Any | None = None
