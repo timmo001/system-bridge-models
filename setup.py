41d49ab09378bf61262d27b75afd85a8f25e96e1
Aidan Timson <aidan@timmo.dev>
2023-12-28 17:37:27 +0000

    Update license to Apache-2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     keywords="system-bridge",
     author="Aidan Timson (Timmo)",
     author_email="aidan@timmo.dev",
-    license="MIT",
+    license="Apache-2.0",
     url="https://github.com/timmo001/system-bridge-models",
     packages=find_packages(exclude=["tests", "generator"]),
     install_requires=requirements,
