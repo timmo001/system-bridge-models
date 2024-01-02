a1f82b16c8293141c2d3508316dc61daa605e3f4
Aidan Timson <aidan@timmo.dev>
2024-01-02 09:43:27 +0000

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,14 @@ with open("requirements_setup.txt", encoding="utf-8") as f:
 with open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
+
 setup(
     name="systembridgemodels",
     description="System Bridge Models",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     keywords="system-bridge",
     author="Aidan Timson (Timmo)",
     author_email="aidan@timmo.dev",
