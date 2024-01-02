276d31523f7e22ac35569895b295ed821af84ba4
Aidan Timson <aidan@timmo.dev>
2024-01-02 14:33:51 +0000

    Refactor model and directory names

--- systembridgemodels/const.py
+++ systembridgemodels/const.py
@@ -2,18 +2,18 @@
 # Model
 from .keyboard_key import KeyboardKey
 from .keyboard_text import KeyboardText
-from .media_directories import MediaDirectories
+from .media_directories import MediaDirectory
 from .media_files import MediaFile, MediaFiles
 from .modules import ModulesData
 from .modules.battery import Battery
 from .modules.cpu import CPU
 from .modules.disks import Disks
-from .modules.displays import Displays
+from .modules.displays import Display
 from .modules.gpus import GPU
 from .modules.media import Media
 from .modules.memory import Memory
 from .modules.networks import Networks
-from .modules.processes import Processes
+from .modules.processes import Process
 from .modules.sensors import Sensors
 from .modules.system import System
 from .notification import Notification
@@ -51,11 +51,11 @@ MODEL_MAP = {
     MODEL_CPU: CPU,
     MODEL_DATA: ModulesData,
     MODEL_DISKS: Disks,
-    MODEL_DISPLAYS: Displays,
+    MODEL_DISPLAYS: list[Display],
     MODEL_GPU: GPU,
     MODEL_KEYBOARD_KEY: KeyboardKey,
     MODEL_KEYBOARD_TEXT: KeyboardText,
-    MODEL_MEDIA_DIRECTORIES: MediaDirectories,
+    MODEL_MEDIA_DIRECTORIES: list[MediaDirectory],
     MODEL_MEDIA_FILE: MediaFile,
     MODEL_MEDIA_FILES: MediaFiles,
     MODEL_MEDIA: Media,
@@ -64,7 +64,7 @@ MODEL_MAP = {
     MODEL_NOTIFICATION: Notification,
     MODEL_OPEN_PATH: OpenPath,
     MODEL_OPEN_URL: OpenUrl,
-    MODEL_PROCESSES: Processes,
+    MODEL_PROCESSES: list[Process],
     MODEL_RESPONSE: Response,
     MODEL_SENSORS: Sensors,
     MODEL_SYSTEM: System,
--- systembridgemodels/media_directories.py
+++ systembridgemodels/media_directories.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Directory:
+class MediaDirectory:
     """Directory."""
 
     key: str
