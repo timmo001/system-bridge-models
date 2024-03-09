2ca00e885262eb7b75d304f4998f5627faae1536
timmo001 <timmo001@users.noreply.github.com>
2024-03-09 12:54:36 +0000

    Bump systembridgemodels version to 4.0.4.dev0

--- systembridgemodels/_version.py
+++ systembridgemodels/_version.py
@@ -7,5 +7,5 @@ Provides systembridgemodels version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgemodels", 4, 0, 3)
+__version__ = Version("systembridgemodels", 4, 0, 4, dev=0)
 __all__ = ["__version__"]
