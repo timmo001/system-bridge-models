2b598387080bb70cd8e9aa2116efe056d9f60ed7
Aidan Timson <aidan@timmo.dev>
2025-08-16 10:02:14 +0100

    Remove incremental, update in sync with connector

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
 """Setup."""
 from setuptools import find_packages, setup
 
-# Get setup packages from requirements.txt
-with open("requirements_setup.txt", encoding="utf-8") as f:
-    requirements_setup = f.read().splitlines()
-
-# Get packages from requirements.txt
-with open("requirements.txt", encoding="utf-8") as f:
-    requirements = f.read().splitlines()
-
 with open("README.md", encoding="utf-8") as f:
     readme = f.read()
 
 setup(
     name="systembridgemodels",
+    version="4.2.5",
     author="Aidan Timson (Timmo)",
     author_email="aidan@timmo.dev",
     description="System Bridge Models",
@@ -22,9 +15,6 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/timmo001/system-bridge-models",
-    install_requires=requirements,
     packages=find_packages(exclude=["tests", "tests.*", "generator"]),
     python_requires=">=3.11",
-    setup_requires=requirements_setup,
-    use_incremental=True,
 )
